f5a1a4f2ca49eb37225f3af8d2d4e8175ce62175
Cihan Akman <cihanozanakman@gmail.com>
2018-11-02 21:03:11 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -17,7 +17,40 @@ WIN_COMBINATIONS = [
   [2,4,6]
 ]
 
-def input_to_index(value)
-  index = value.to_i - 1
-  return index
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
+
+def move(board, index, current_player = "X")
+  board[index] = current_player
+end
+
+def position_taken?(board, location)
+  board[location] != " " && board[location] != ""
+end
+
+def valid_move?(board, index)
+  index.between?(0,8) && !position_taken?(board, index)
+end
+
+def turn(board, counter)
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+  if valid_move?(board, index)
+    move(board, index)
+    counter += 1
+    display_board(board)
+    return counter
+  else
+    turn(board, counter)
+  end
+end
+
+# Define your play method below
+def play(board)
+counter = 0
+  while counter < 9
+    counter = turn(board,counter)
+  end
 end
